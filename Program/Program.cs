982f1891e5eb5985be1ef76266fec34353ef31ad
florinleon <florinleon@gmail.com>
2023-04-27 19:12:29 +0300

    Initial
    Versiunea 1

---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GitHubTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(MyMath.Add(1, 2));
+        }
+    }
+
+    public class MyMath
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
